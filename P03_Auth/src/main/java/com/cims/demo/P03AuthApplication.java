8e006ce8fe8af4ef7eefdeb593b3f876ef2e8e89
JatinDumbre <50858742+JatinDumbre@users.noreply.github.com>
2025-01-30 18:58:17 +0530

    Initial commit of authentication (#2)
    Co-authored-by: Prerakghatode <prerakghatode4@gmail.com>

---
+++ P03_Auth/src/main/java/com/cims/demo/P03AuthApplication.java
@@ -0,0 +1,13 @@
+package com.cims.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P03AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P03AuthApplication.class, args);
+	}
+
+}
