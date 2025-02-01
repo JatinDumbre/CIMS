5bbd248aac832b7637fd2f3e450cb4976e188b5f
Prerakghatode <136691583+Prerakghatode@users.noreply.github.com>
2025-02-01 12:13:42 +0530

    Authentication Microservice Changes  (#7)
    * Initial commit of authentication
    
    * Create DemoCheck.txt
    
    * Authentication Changes

---
+++ P03_Auth/P03_Auth/src/main/java/Classes/UserLogin.java
@@ -0,0 +1,13 @@
+package Classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserLogin {
+	String email;
+	String password;
+}
---
+++ P03_Auth/P03_Auth/src/test/java/com/cims/auth/P03AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cims.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P03AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
